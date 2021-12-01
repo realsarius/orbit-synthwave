f0fb34f767691c1ab82c7d048be38cffcfe5ba1e
realsarius <tehadro@protonmail.com>
2021-12-01 15:17:31 +0300

    Switched from strict to browser router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from "react-router-dom";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap';
 import 'fontawesome/index';
@@ -9,13 +9,8 @@ import './css/style.css';
 import './js/app';
 
 ReactDOM.render(
-    <React.StrictMode>
+    <BrowserRouter>
         <App/>
-    </React.StrictMode>,
+    </BrowserRouter>,
     document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
