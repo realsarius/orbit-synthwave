707cab33c4fb8c61473d57930a22525df69acd88
realsarius <tehadro@protonmail.com>
2021-12-01 15:16:45 +0300

    New nav links added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Route, Switch, Link} from 'react-router-dom';
 import Header from "./components/Header";
 import Introduction from "./components/Introduction";
 import NavBar from "./components/NavBar";
@@ -9,26 +9,63 @@ import Footer from "./components/Footer";
 import Videos from "./components/Videos";
 import Music from "./components/Music";
 import Services from "./components/Services";
+import AddReview from "./components/add-review";
+import Restaurant from "./components/restaurants"
+import RestaurantsList from "./components/restaurants-list";
+import Login from "./components/login";
 
 const App = () => {
+    const [user, setUser] = React.useState(null);
+
+    async function login(user = null) {
+        setUser(user);
+    }
+
+    async function logout() {
+        setUser(null)
+    }
+
     return (
-        <Router>
-            <div>
-                <NavBar/>
-                <Route path={'/'} exact render={(props) => (
-                    <>
-                        <Header/>
-                        <Services/>
-                        <Introduction/>
-                        <News/>
-                        <Videos/>
-                        <Music/>
-                    </>
-                )}/>
-                <Route path={'/about'} component={About}/>
-                <Footer/>
-            </div>
-        </Router>
+        <div>
+            <NavBar/>
+            <Route path={'/'} exact render={(props) => (
+                <>
+                    <Header/>
+                    <Services/>
+                    <Introduction/>
+                    <News/>
+                    <Videos/>
+                    <Music/>
+                </>
+            )}/>
+            <Route path={'/restaurants'} exact render={(props) => (
+                <div className="container mt-3 pt-5">
+                    <Switch>
+                        <Route exact path={["/", "/restaurants"]} component={RestaurantsList}/>
+                        <Route
+                            path="/restaurants/:id/review"
+                            render={(props) => (
+                                <AddReview {...props} user={user}/>
+                            )}
+                        />
+                        <Route
+                            path="/restaurants/:id"
+                            render={(props) => (
+                                <Restaurant {...props} user={user}/>
+                            )}
+                        />
+                        <Route
+                            path="/login"
+                            render={(props) => (
+                                <Login {...props} login={login}/>
+                            )}
+                        />
+                    </Switch>
+                </div>
+            )}/>
+            <Route path={'/about'} component={About}/>
+            <Footer/>
+        </div>
     );
 };
 
