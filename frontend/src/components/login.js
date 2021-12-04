a04446f8e22fb572377ec55779a10347177a4764
realsarius <tehadro@protonmail.com>
2021-12-04 19:24:24 +0300

    fixed the login page

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -21,41 +21,49 @@ const Login = props => {
 
     return (
         <div className={"p-5"}>
-            <div className={"p-5 login-top-padding"}>
-                <div className={"submit-form p-5"}>
-                    <div>
-                        <div className="form-group">
-                            <label htmlFor="user">Username</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                id="name"
-                                required
-                                value={user.name}
-                                onChange={handleInputChange}
-                                name="name"
-                            />
-                        </div>
+            <div className={'container pt-5'}>
+                <div className={'row justify-content-center align-content-center pt-5'}>
+                    <div className={'col-md-6 pt-5'}>
+                        <h1 className={'text-center'}>LOGIN</h1>
+                        <div className={"p-5 login-top-padding"}>
+                            <div className={"submit-form"}>
+                                <div>
+                                    <div className="form-group">
+                                        <label htmlFor="user">Username</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            id="name"
+                                            required
+                                            value={user.name}
+                                            onChange={handleInputChange}
+                                            name="name"
+                                        />
+                                    </div>
 
-                        <div className="form-group">
-                            <label htmlFor="id">ID</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                id="id"
-                                required
-                                value={user.id}
-                                onChange={handleInputChange}
-                                name="id"
-                            />
-                        </div>
+                                    <div className="form-group pt-3">
+                                        <label htmlFor="id">Password</label>
+                                        <input
+                                            type="password"
+                                            className="form-control"
+                                            id="id"
+                                            required
+                                            value={user.id}
+                                            onChange={handleInputChange}
+                                            name="id"
+                                        />
+                                    </div>
 
-                        <button onClick={login} className="btn btn-success">
-                            Login
-                        </button>
+                                    <button onClick={login} className="btn btn-success mt-4 text-black w-100">
+                                        Login
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+
         </div>
 
 
