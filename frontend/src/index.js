02012568ea123b1a5fdf9dda7d32a0a8f060e60d
realsarius <tehadro@protonmail.com>
2022-03-14 14:04:26 +0300

    import custom bootstrap css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap';
+import './css/bootstrap.css'
 import 'fontawesome/index';
 import './css/style.css';
 import './js/app';
+import {AuthProvider} from "./context/AuthProvider";
 
 ReactDOM.render(
     <BrowserRouter>
